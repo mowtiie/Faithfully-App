a7789d4912a14662ca9969d962b49c9c8461050b
mowtiie <mowtiie.dev@gmail.com>
2026-06-09 14:02:29 +0800

    Moved photo viewer caption to toolbar title

--- app/src/main/java/com/mowtiie/faithfully/ui/activities/PhotoViewerActivity.java
+++ app/src/main/java/com/mowtiie/faithfully/ui/activities/PhotoViewerActivity.java
@@ -1,6 +1,7 @@
 package com.mowtiie.faithfully.ui.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +9,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
 import com.mowtiie.faithfully.R;
+import com.mowtiie.faithfully.databinding.ActivityPhotoViewerBinding;
 
 public class PhotoViewerActivity extends AppCompatActivity {
+
+    private ActivityPhotoViewerBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_photo_viewer);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        binding = ActivityPhotoViewerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setSupportActionBar(binding.toolbar);
+
+        String imageUrl = getIntent().getStringExtra("image_url");
+        String caption  = getIntent().getStringExtra("caption");
+
+        assert caption != null;
+        binding.toolbar.setTitle(caption.isEmpty() ? "Photo Viewer" : caption);
+
+        Glide.with(this).load(imageUrl).fitCenter().into(binding.ivFull);
+        binding.toolbar.setNavigationOnClickListener(v -> finish());
     }
 }
